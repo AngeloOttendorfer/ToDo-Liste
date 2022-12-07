4312584e1f882e1defc5c364c0c80d872be1aac7
Angelo Ottendorfer <ic21b113@technikum-wien.at>
2022-12-07 15:37:25 +0100

    Managed to fill the task column in the tableView using cellFactory in the application class and StringProperty in the task class. Changed Files: HelloApplication.java, Task.java

--- src/main/java/com/example/todoliste/HelloApplication.java
+++ src/main/java/com/example/todoliste/HelloApplication.java
@@ -2,10 +2,12 @@ package com.example.todoliste;
 
 import com.example.todoliste.model.Task;
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,52 +17,53 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        SplitPane splitPane = new SplitPane();//two-sided gui
+        Pane pane = new Pane();
         VBox input = new VBox();
-        VBox tasks = new VBox();
-        VBox dates = new VBox();
-        VBox priorities = new VBox();
         TextField tf_task = new TextField();
         Label lbl_task = new Label("TASK");
         Label lbl_date = new Label("DEADLINE");
         TextField tf_date = new TextField();
-        TextArea ta_priority = new TextArea();
         Button btn_add = new Button("ADD");
         Button btn_clear = new Button("CLEAR");
         BorderPane borderPane = new BorderPane();
         TableView<Task> tView = new TableView<Task>();
-        TableColumn<Task, String> taskCol = new TableColumn<Task, String>("Task");
-        TableColumn<Task, String> dateCol = new TableColumn<Task, String>("Date");
+        TableColumn<Task, String> taskCol = new TableColumn<>("Task");
+        TableColumn<Task, String> dateCol = new TableColumn<>("Date");
+        TableColumn<Task, String> priorityCol = new TableColumn<>("Priority");
+        TableColumn<Task, String> statusCol = new TableColumn<>("Status");
 
+        final ObservableList<Task> todo = FXCollections.observableArrayList();
+
+        pane.getChildren().addAll(input, borderPane);
 
         input.getChildren().addAll(lbl_task, tf_task, lbl_date, tf_date, btn_add, btn_clear);
-        splitPane.getItems().addAll(input, tasks, dates, priorities);
-
+        borderPane.setLeft(input);
+        borderPane.setCenter(tView);
 
         btn_add.setPrefWidth(100);
         btn_clear.setPrefWidth(100);
 
         tView.setEditable(true);
-        tView.getColumns().addAll(taskCol, dateCol);
-
+        tView.getColumns().addAll(taskCol, dateCol, priorityCol, statusCol);
 
         btn_add.setOnAction(e ->{
             String task = tf_task.getText();
             String date = tf_date.getText();
             Task newTask = new Task(task, date);
-            CheckBox check = new CheckBox(newTask.getTask());
-            TextArea ta_date = new TextArea();
-            ta_date.setPadding(new Insets(20, 10, 20, 10));
-            ta_date.appendText(newTask.getDate());
-            tasks.getChildren().add(check);
-            dates.getChildren().add(ta_date);
+            //ta_date.setPadding(new Insets(20, 10, 20, 10));
+            todo.add(newTask);
+            taskCol.setCellValueFactory((p) -> {
+                return p.getValue().taskProperty();
+            });
         });
 
         btn_clear.setOnAction(e ->{
-            tasks.getChildren().clear();
+            todo.removeAll(tView.getItems());
         });
 
-        Scene scene = new Scene(splitPane);
+        tView.setItems(todo);
+
+        Scene scene = new Scene(pane);
         stage.setTitle("ToDo-List!");
         stage.setScene(scene);
         stage.setMinWidth(500);
--- src/main/java/com/example/todoliste/model/Task.java
+++ src/main/java/com/example/todoliste/model/Task.java
@@ -1,28 +1,45 @@
 package com.example.todoliste.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Task {
-    private String task;
-    private String date;
+    private StringProperty task;
+    private StringProperty date;
 
     public Task(String task, String date){
-        this.task = task;
-        this.date = date;
+        this.task = new SimpleStringProperty(task);
+        this.date = new SimpleStringProperty(date);
     }
 
     public String getTask() {
-        return task;
+        return task.get();
     }
 
     public void setTask(String task) {
-        this.task = task;
+        this.task.set(task);
     }
 
     public String getDate() {
-        return date;
+        return date.get();
     }
 
     public void setDate(String date) {
-        this.date = date;
+        this.date.set(date);
+    }
+
+    public StringProperty taskProperty(){
+        if(task == null){
+            task = new SimpleStringProperty("-");
+        }
+        return task;
+    }
+
+    public StringProperty dateProperty(){
+        if(date == null){
+            date = new SimpleStringProperty("-");
+        }
+        return date;
     }
 
     @Override
