6909ada5df54cb840070a684c0da085ad818f943
Angelo Ottendorfer <ic21b113@technikum-wien.at>
2022-12-28 15:17:08 +0100

    Implemented a new design for the GUI, an Alert message occurs, when not all task fields are filled with strings. Gui expanded to day, time and task description property. Problem: When a value in the combobox is set and afterwards a new task is added to the list, all values that have been already specified in the combobox vanish.

--- src/main/java/com/example/todoliste/application/ToDoApplication.java
+++ src/main/java/com/example/todoliste/application/ToDoApplication.java
@@ -11,8 +11,8 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,17 +23,21 @@ public class ToDoApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         Pane pane = new Pane();
-        VBox input = new VBox();
+        HBox input = new HBox();
         TextField tf_task = new TextField();
-        Label lbl_task = new Label("TASK");
-        Label lbl_date = new Label("DEADLINE");
+        TextField tf_day = new TextField();
+        TextField tf_time = new TextField();
         TextField tf_date = new TextField();
+        TextField tf_info = new TextField();
         Button btn_add = new Button("ADD");
         Button btn_clear = new Button("CLEAR");
         BorderPane borderPane = new BorderPane();
         TableView<Task> tView = new TableView<>();
         TableColumn<Task, String> taskCol = new TableColumn<>("Task");
-        TableColumn<Task, String> dateCol = new TableColumn<>("Date");
+        TableColumn<Task, String> dayCol = new TableColumn<>("Day");
+        TableColumn<Task, String> timeCol = new TableColumn<>("Time");
+        TableColumn<Task, String> dateCol = new TableColumn<>("Deadline");
+        TableColumn<Task, String> infoCol = new TableColumn<>("Description");
         TableColumn<Task, IntegerProperty> priorityCol = new TableColumn<>("Priority");
         TableColumn<Task, StringProperty> statusCol = new TableColumn<>("Status");
         ObservableList<String> options = FXCollections.observableArrayList("Done", "In progress", "Backlog");
@@ -44,55 +48,90 @@ public class ToDoApplication extends Application {
 
         pane.getChildren().addAll(input, borderPane);
 
-        input.getChildren().addAll(lbl_task, tf_task, lbl_date, tf_date, btn_add, btn_clear);
-        borderPane.setLeft(input);
+        input.getChildren().addAll(tf_task, tf_day, tf_time, tf_date, tf_info, btn_add, btn_clear);
         borderPane.setCenter(tView);
+        borderPane.setBottom(input);
 
+
+        //tView.setPrefWidth(900);
+        tf_task.setPromptText("Task name");
+        tf_day.setPromptText("Expiration day");
+        tf_time.setPromptText("Expiration time");
+        tf_date.setPromptText("Expiration date");
+        tf_info.setPromptText("Add specific descriptions");
         btn_add.setPrefWidth(100);
         btn_clear.setPrefWidth(100);
         taskCol.setPrefWidth(200);
+        dayCol.setPrefWidth(200);
+        timeCol.setPrefWidth(200);
         dateCol.setPrefWidth(200);
+        infoCol.setPrefWidth(200);
         statusCol.setPrefWidth(200);
         priorityCol.setPrefWidth(200);
 
         tView.setEditable(true);
-        tView.getColumns().addAll(taskCol, dateCol, priorityCol, statusCol);
+        tView.getColumns().addAll(taskCol, dayCol, timeCol, dateCol, infoCol, priorityCol, statusCol);
+        tView.setOnMouseClicked(event -> tView.getSelectionModel().clearSelection());
 
 
         btn_add.setOnAction(e ->{
             String task = tf_task.getText();
+            String day = tf_day.getText();
+            String time = tf_time.getText();
             String date = tf_date.getText();
-            Task newTask = new Task(task, date);
-            //ta_date.setPadding(new Insets(20, 10, 20, 10));
-            todo.add(newTask);
-            taskCol.setCellValueFactory((p) -> p.getValue().taskProperty()); //return
-            dateCol.setCellValueFactory((p) -> p.getValue().dateProperty()); //return
+            String info = tf_info.getText();
+            if (tf_task.getText().isEmpty() || tf_day.getText().isEmpty() || tf_time.getText().isEmpty() || tf_date.getText().isEmpty() || tf_info.getText().isEmpty()) {
+                // Zeige eine Meldung an, wenn nicht alle Felder ausgefüllt sind
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("All fields must be filled");
+                alert.setContentText("Please fill in all fields before adding a task.");
+                alert.showAndWait();
+            }
+            else{
+                Task newTask = new Task(task, day, time, date, info);
+                //ta_date.setPadding(new Insets(20, 10, 20, 10));
+                todo.add(newTask);
+                taskCol.setCellValueFactory((p) -> p.getValue().taskProperty()); //return
+                dayCol.setCellValueFactory((p) -> p.getValue().dayProperty()); //return
+                timeCol.setCellValueFactory((p) -> p.getValue().timeProperty()); //return
+                dateCol.setCellValueFactory((p) -> p.getValue().dateProperty()); //return
+                infoCol.setCellValueFactory((p) -> p.getValue().infoProperty()); // return
 
-            statusCol.setCellFactory((p)->{
-                TableCell<Task, StringProperty> status = new TableCell<>();
-                final ComboBox<String> combo_status = new ComboBox<>(options);
-                combo_status.prefWidthProperty().bind(statusCol.widthProperty().subtract(2));
-                status.graphicProperty().bind(Bindings.when(status.emptyProperty()).then((Node) null).otherwise(combo_status));
-                return status;
-            });
+                statusCol.setCellFactory((p)->{
+                    TableCell<Task, StringProperty> status = new TableCell<>();
+                    final ComboBox<String> combo_status = new ComboBox<>(options);
+                    combo_status.setStyle("-fx-background-color: white");
+                    combo_status.prefWidthProperty().bind(statusCol.widthProperty().subtract(2));
+                    status.graphicProperty().bind(Bindings.when(status.emptyProperty()).then((Node) null).otherwise(combo_status));
+                    return status;
+                });
 
-            numTasks.incrementAndGet();
-            priorityCol.setCellFactory((p) ->{
-                TableCell<Task, IntegerProperty> priority = new TableCell<>();
-                final ComboBox<Integer> combo_priority = new ComboBox<>(priorities);
-                for (int i = 1; i <= numTasks.get(); i++) {
-                    if(!priorities.contains(i)){
-                        priorities.add(i);
+                numTasks.incrementAndGet();
+                priorityCol.setCellFactory((p) ->{
+                    TableCell<Task, IntegerProperty> priority = new TableCell<>();
+                    final ComboBox<Integer> combo_priority = new ComboBox<>(priorities);
+                    combo_priority.setStyle("-fx-background-color: white");
+                    for (int i = 1; i <= numTasks.get(); i++) {
+                        if(!priorities.contains(i)){
+                            priorities.add(i);
+                        }
                     }
-                }
-                combo_priority.prefWidthProperty().bind(statusCol.widthProperty().subtract(2));
-                priority.graphicProperty().bind(Bindings.when(priority.emptyProperty()).then((Node) null).otherwise(combo_priority));
-                return priority;
-            });
-            tView.setItems(todo);
-            tView.getSelectionModel().select(newTask);
-            tf_task.clear();
-            tf_date.clear();
+                    combo_priority.prefWidthProperty().bind(statusCol.widthProperty().subtract(2));
+                    priority.graphicProperty().bind(Bindings.when(priority.emptyProperty()).then((Node) null).otherwise(combo_priority));
+                    return priority;
+                });
+                tView.setItems(todo);
+                tView.getSelectionModel().select(newTask);
+                tf_task.clear();
+                tf_day.clear();
+                tf_time.clear();
+                tf_date.clear();
+                tf_info.clear();
+                // Setze den Prompt Text für tf_task und tf_date wieder auf den ursprünglichen Wert
+                tf_task.setPromptText("Enter task");
+                tf_date.setPromptText("Enter deadline");
+            }
         });
 
 
--- src/main/java/com/example/todoliste/model/Task.java
+++ src/main/java/com/example/todoliste/model/Task.java
@@ -5,13 +5,19 @@ import javafx.beans.property.StringProperty;
 
 public class Task {
     private StringProperty task;
+    private StringProperty day;
+    private StringProperty time;
     private StringProperty date;
+    private StringProperty info;
 
     private final StringProperty status = new SimpleStringProperty();
 
-    public Task(String task, String date){
+    public Task(String task, String day, String time, String date, String info){
         this.task = new SimpleStringProperty(task);
+        this.day = new SimpleStringProperty(day);
+        this.time = new SimpleStringProperty(time);
         this.date = new SimpleStringProperty(date);
+        this.info = new SimpleStringProperty(info);
     }
 
     public String getTask() {
@@ -22,12 +28,36 @@ public class Task {
         this.task.set(task);
     }
 
+    public String getDay() {
+        return day.get();
+    }
+
+    public void setDay(String day) {
+        this.day.set(day);
+    }
+
+    public String getTime() {
+        return time.get();
+    }
+
+    public void setTime(String time) {
+        this.time.set(time);
+    }
+
     public String getDate() {
         return date.get();
     }
 
     public void setDate(String date) {
         this.date.set(date);
+    }
+
+    public String getInfo() {
+        return info.get();
+    }
+
+    public void setInfo(String info) {
+        this.info.set(info);
     }
 
     public void setStatus(String value){
@@ -45,11 +75,32 @@ public class Task {
         return task;
     }
 
+    public StringProperty dayProperty(){
+        if(day == null){
+            day = new SimpleStringProperty("-");
+        }
+        return day;
+    }
+
+    public StringProperty timeProperty(){
+        if(time == null){
+            time = new SimpleStringProperty("-");
+        }
+        return time;
+    }
+
     public StringProperty dateProperty(){
         if(date == null){
             date = new SimpleStringProperty("-");
         }
         return date;
+    }
+
+    public StringProperty infoProperty(){
+        if(info == null){
+            info = new SimpleStringProperty("-");
+        }
+        return info;
     }
 
     public StringProperty statusProperty(){
